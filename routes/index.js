86b0a0230b13ee1b639bc37b9b8835e2e62cb0ef
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-04-04 11:39:57 -0400

    Add handlers for the GET and POST methods

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+
+const messages = [
+    {
+        text: 'Hi there!',
+        user: 'Amando',
+        added: new Date(),
+    },
+    {
+        text: 'Hello World!',
+        user: 'Charles',
+        added: new Date(),
+    },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Mini Messageboard', messages });
+});
+
+router.get('/new', function (req, res, next) {
+    res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+    const newMessage = {
+        user: req.body.author,
+        text: req.body.message,
+        added: new Date(),
+    };
+    messages.push(newMessage);
+    res.redirect('/');
 });
 
 module.exports = router;
